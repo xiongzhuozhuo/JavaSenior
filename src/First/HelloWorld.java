3c69011599f6b2e4569790a10981f05718ea0c83
xiong <68106285+xiongzhuozhuo@users.noreply.github.com>
2021-08-26 10:25:04 +0800

    Update HelloWorld.java

--- src/First/HelloWorld.java
+++ src/First/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("hello world");
         System.out.println("GUET");
         System.out.println("添加一行");
+        System.out.println("从github添加一行");
     }
 }
